48927ae1aea0f8d0dd29856206ccccdcd523fbbb
cdixi001 <cdixi001@users.noreply.github.com>
2015-06-02 22:02:02 -0700

    compiles

--- finalproj.c
+++ finalproj.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
-#include "./path/io.c"
-#include "./path/io.h"
 
 //-------------------------------------------------------------------------------------
 // macro for easier usage
@@ -98,7 +96,149 @@ void delay_ms(int miliSec) //for 8 Mhz crystal
 
 //~~~~~~~~~~~~~~~~~~END LCD STUFF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
 
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
+
+unsigned char GetKeypadKey() {
+	// Check keys in col 1
+	// Enable col 4 with 0, disable others with 1?s
+	// The delay allows PORTC to stabilize before checking
+	PORTC = 0xEF;
+	asm("nop");
+	if (GetBit(PINC,0)==0) { return('D'); }
+	if (GetBit(PINC,1)==0) { return('C'); }
+	if (GetBit(PINC,2)==0) { return('B'); }
+	if (GetBit(PINC,3)==0) { return('A'); }
+	// Check keys in col 2
+	// Enable col 5 with 0, disable others with 1?s
+	// The delay allows PORTC to stabilize before checking
+	PORTC = 0xDF;
+	asm("nop");
+	if (GetBit(PINC,0)==0) { return('#'); }
+	if (GetBit(PINC,1)==0) { return('9'); }
+	if (GetBit(PINC,2)==0) { return('6'); }
+	if (GetBit(PINC,3)==0) { return('3'); }
+	// ... *****FINISH*****
+	// Check keys in col 3
+	// Enable col 6 with 0, disable others with 1?s
+	// The delay allows PORTC to stabilize before checking
+	PORTC = 0xBF;
+	asm("nop");
+	if (GetBit(PINC,0)==0) { return('0'); }
+	if (GetBit(PINC,1)==0) { return('8'); }
+	if (GetBit(PINC,2)==0) { return('5'); }
+	if (GetBit(PINC,3)==0) { return('2'); }
+	// ... *****FINISH*****
+	// Check keys in col 4
+	// ... *****FINISH*****
+	PORTC = 0x7F;
+	asm("nop");
+	if (GetBit(PINC,0)==0) { return('*'); }
+	if (GetBit(PINC,1)==0) { return('7'); }
+	if (GetBit(PINC,2)==0) { return('4'); }
+	if (GetBit(PINC,3)==0) { return('1'); }
+	return('\0'); // default value
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~TIMMER STUFF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// TimerISR() sets this to 1. C programmer should clear to 0.
+volatile unsigned char TimerFlag = 0;
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+	TCCR1B = 0x0B;
+	// AVR output compare register OCR1A.
+	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	OCR1A = 125;// AVR timer interrupt mask register
+	// bit1: OCIE1A -- enables compare match interrupt
+	TIMSK1 = 0x02;
+	//Initialize avr counter
+	TCNT1=0;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+	_avr_timer_cntcurr = _avr_timer_M;
+	//Enable global interrupts: 0x80: 1000000
+	SREG |= 0x80;
+}
+void TimerOff() {
+	// bit3bit1bit0=000: timer off
+	TCCR1B = 0x00;
+}
+void TimerISR() {
+	TimerFlag = 1;
+}
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1
+	// (every 1 ms per TimerOn settings)
+	// Count down to 0 rather than up to TOP (results in a more efficient comparison)
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		// Call the ISR that the user uses
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+void set_PWM(double frequency) {
+	// Keeps track of the currently set frequency
+	// Will only update the registers when the frequency
+	// changes, plays music uninterrupted.
+	static double current_frequency;
+	if (frequency != current_frequency) {
+
+		if (!frequency) TCCR3B &= 0x08; //stops timer/counter
+		else TCCR3B |= 0x03; // resumes/continues timer/counter
+		
+		// prevents OCR3A from overflowing, using prescaler 64
+		// 0.954 is smallest frequency that will not result in overflow
+		if (frequency < 0.954) OCR3A = 0xFFFF;
+		
+		// prevents OCR3A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250) OCR3A = 0x0000;
+		
+		// set OCR3A based on desired frequency
+		else OCR3A = (short)(8000000 / (128 * frequency)) - 1;
+
+		TCNT3 = 0; // resets counter
+		current_frequency = frequency;
+	}
+}
+
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	// COM3A0: Toggle PB6 on compare match between counter and OCR3A
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	// WGM32: When counter (TCNT3) matches OCR3A, reset counter
+	// CS31 & CS30: Set a prescaler of 64
+	set_PWM(0);
+}
+
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~END TIMER STUFF~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
@@ -110,6 +250,7 @@ char p1LED = 0;
 char p2LED = 0;
 char recordflag = 0;
 char compareflag = 0;
+char playbackflag = 0;
 char endchar = 'D';
 double song1[1];
 double song2[2];
@@ -123,6 +264,9 @@ unsigned char p2score = 0;
 char winflag = 0;
 char gameflag = 1;
 char singleflag = 1;
+
+char button1 = 0;
+char button2 = 0;
 
 
 double getfreq(char fig) {
@@ -186,8 +330,6 @@ int TickFct_record(int state) {
 	/*VARIABLES MUST BE DECLARED STATIC*/
 	/*e.g., static int x = 0;*/
 	/*Define user variables for this state machine here. No functions; make them global.*/
-	static char i = 0;
-
 	static char i = 0;
 	switch(state) { // Transitions
 		case -1:
@@ -330,7 +472,7 @@ int TickFct_compare(int state) {
 	/*VARIABLES MUST BE DECLARED STATIC*/
 	/*e.g., static int x = 0;*/
 	/*Define user variables for this state machine here. No functions; make them global.*/
-
+	static char i = 0;
 	switch(state) { // Transitions
 		case -1:
 			state = comp_init;
@@ -346,14 +488,14 @@ int TickFct_compare(int state) {
 			}
 			break;
 		case comp_waitPress:
-			if (GetKeypadKey() == '\0';) {
+			if (GetKeypadKey() == '\0') {
 				state = comp_waitPress;
 			}
-			else if (GetKeypadKey() == arr[i];) {
+			else if (GetKeypadKey() == thesong[i]) {
 				state = comp_waitRelease;
 				i++;
 			}
-			else if (GetKeypadKey() != arr[i];) {
+			else if (GetKeypadKey() != thesong[i]) {
 				state = comp_youLose;
 			}
 			break;
@@ -418,14 +560,14 @@ int TickFct_menu(int state) {
 			if (!gameflag) {
 				state = menu_init;
 			}
-			else if (//BUTTON PRESS) {
+			else if (button1) {
 				state = menu_single;
 				singleflag = 1;
 				winflag = 1;
 				songcount = 0;
 				p2LED = 0;
 			}
-			else if (//BUTTON PRESS) {
+			else if (button2) {
 				state = menu_multi;
 				singleflag = 0;
 				winflag = 0;
@@ -463,7 +605,7 @@ int TickFct_menu(int state) {
 			break;
 		case menu_singlewin:
 			count = 0;
-			state = menu_waitfinsh;
+			state = menu_waitfinish;
 			break;
 		case menu_wait2compare:
 			if (compareflag) {
@@ -475,11 +617,11 @@ int TickFct_menu(int state) {
 			break;
 		case menu_singlelose:
 			count = 0;
-			state = menu_waitfinsh;
+			state = menu_waitfinish;
 			break;
 		case menu_multiwin:
 			count = 0;
-			state = menu_waitfinsh;
+			state = menu_waitfinish;
 			break;
 		case menu_waitfinish:
 			if(count < 50) {
@@ -521,17 +663,17 @@ int TickFct_menu(int state) {
 			break;
 		case menu_singlewin:
 			LCD_ClearScren();
-			LCD_DisplayString("Congratulations you win!!");
+			LCD_DisplayString(0, "Congratulations you win!!");
 			break;
 		case menu_wait2compare:
 			break;
 		case menu_singlelose:
 			LCD_ClearScren();
-			LCD_DisplayString("You lose, inferior.");
+			LCD_DisplayString(0, "You lose, inferior.");
 			break;
 		case menu_multiwin:
 			LCD_ClearScreen();
-			LCD_DisplayString("P   Wins!!!);
+			LCD_DisplayString(0, "P   Wins!!!");
 			LCD_Cursor(1);
 			if(p1score > 4) {
 				LCD_WriteData("1");
@@ -584,6 +726,7 @@ int main(void)
 	DDRB = 0xFF; PORTB = 0x00; // LED's
 	DDRD = 0xFF; PORTD = 0x00; // LCD
 	DDRC = 0xF0; PORTC = 0x0F; // keypad
+	char pinna = PINA;
 	LCD_init();
 	// Starting at position 1 on the LCD screen
 	//LCD_DisplayString(1, "button menu section");
@@ -610,6 +753,16 @@ int main(void)
 			}
 			tasks[i].elapsedTime += 1;
 		}
+		
+		pinna = PINA;
+		pinna = ~pinna;
+		
+		if(GetBit(pinna, 0)) {
+			button1 = 1;
+		} else button1 = 0;
+		if(GetBit(pinna, 1)) {
+			button2 = 1;
+		} else button2 = 0;
 		
 		while(!TimerFlag);
 		TimerFlag = 0;
